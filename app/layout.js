5258d10c46337786fc00e425701fe7b9d8a36ebd
khaledcrcj <khaledhesham@live.com>
2026-03-19 18:02:03 +0400

    layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,7 @@
-export const metadata = {
-  title: "CRCJ",
-  description: "Cairo Regional Center for Criminal Justice",
-};
-
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body style={{ fontFamily: "Arial, sans-serif", margin: 0 }}>
-        {children}
-      </body>
+    <html>
+      <body>{children}</body>
     </html>
   );
 }
