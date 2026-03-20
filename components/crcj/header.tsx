0c8e4b0ac2a121ffc63e7718435fb627df3a7fb1
khaledcrcj <khaledhesham@live.com>
2026-03-20 18:13:16 +0400

    header.tsx

--- components/crcj/header.tsx
+++ components/crcj/header.tsx
@@ -99,6 +99,7 @@ export function Header({ lang, setLang }: HeaderProps) {
             </button>
           </div>
 
+          {/* Mobile Menu Button */}
           <button
             type="button"
             onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
