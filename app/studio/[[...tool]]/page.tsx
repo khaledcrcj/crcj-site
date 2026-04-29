a2daa538fd605f982ddaad23fcf21459e7889e95
khaledcrcj <khaledhesham@live.com>
2026-04-29 09:03:16 +0400

    Fix studio browser loading

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -1,10 +1,13 @@
-import { NextStudio } from "next-sanity/studio";
+"use client";
+
+import dynamic from "next/dynamic";
 import config from "@/sanity.config";
 import { projectId } from "@/sanity.env";
 
-export const dynamic = "force-static";
-
-export { metadata, viewport } from "next-sanity/studio";
+const NextStudio = dynamic(
+  () => import("next-sanity/studio").then((mod) => mod.NextStudio),
+  { ssr: false }
+);
 
 export default function StudioPage() {
   if (!projectId) {
